698f9f7ebab8a4738f29be595a84207e547b8128
Matheus Henrique <matheus.mhsantos@mercadolivre.com>
2022-01-10 16:30:35 -0300

    Project create

---
+++ backend/src/main/java/com/mercadolivre/src/DsMovieApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mercadolivre/src/DsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
